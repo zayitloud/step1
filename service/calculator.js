c1d259c7b8dc9e2fb5d1ac24bede2a114c397bec
zayitloud <williez@gmail.com>
2020-02-10 23:27:31 +0100

    beauty

--- service/calculator.js
+++ service/calculator.js
@@ -6,6 +6,6 @@ const isMultipleOf = (value, multiple) => value % multiple == 0
 const getWord = (value) => {
     if (isNaN(value)) throw new Error(`Invalid number provided ${value}`)
     const word = multiples.map((mul, i) => isMultipleOf(value, mul) ? words[i] : NO_MATCH).filter(w => w != NO_MATCH)
-    return word ? (word.length == 0 ? value : word[0]) : value
+    return !word || word.length == 0 ? value : word[0]
 }
 module.exports = { getWord }
