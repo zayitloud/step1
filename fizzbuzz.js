d84223b1f3483340d417315a2aefb4308e4426eb
zayitloud <williez@gmail.com>
2020-02-10 22:57:55 +0100

    code refactroing

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,17 +1,2 @@
-const calc = require('./service/calculator.js')
-if (process.argv.length != 4) {
-    console.log('Invalid arguments passed')
-    return
-}
-const args = process.argv.slice(2).map(n => parseInt(n))
-if (isNaN(args[0]) || isNaN(args[1])) {
-    console.log('Invalid number(s) provided')
-    return
-}
-const numbers = []
-for (let i = args[0]; i <= args[1]; i++) {
-    numbers.push(i)
-}
-console.log(numbers.map(calc.getWord).join(' '))
-
-
+const runner = require('./service/runner')
+console.log(runner.run(process.argv))
